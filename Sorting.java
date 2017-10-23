4ec622729b7b4c1ab13a3dc386e8fbd796e6aad9
brinkley182 <donulfig@gmail.com>
2017-10-22 20:51:31 -0500

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -4,6 +4,78 @@ import java.util.Random;
 
 public class Sorting 
 {
+	 void merge(int arr[], int l, int m, int r)
+	    {
+	        // Find sizes of two subarrays to be merged
+	        int n1 = m - l + 1;
+	        int n2 = r - m;
+	 
+	        // Create temp arrays 
+	        int L[] = new int [n1];
+	        int R[] = new int [n2];
+	 
+	        // Copies data into temp array
+	        for (int i=0; i<n1; ++i)
+	            L[i] = arr[l + i];
+	        for (int j=0; j<n2; ++j)
+	            R[j] = arr[m + 1+ j];
+	 
+	 
+	        // Merges the temp arrays
+	 
+	        
+	        int i = 0, j = 0;
+	 
+	        
+	        int k = l;
+	        while (i < n1 && j < n2)
+	        {
+	            if (L[i] <= R[j])
+	            {
+	                arr[k] = L[i];
+	                i++;
+	            }
+	            else
+	            {
+	                arr[k] = R[j];
+	                j++;
+	            }
+	            k++;
+	        }
+	 
+	        // Copies remaining elements if there are any
+	        while (i < n1)
+	        {
+	            arr[k] = L[i];
+	            i++;
+	            k++;
+	        }
+	 
+	        
+	        while (j < n2)
+	        {
+	            arr[k] = R[j];
+	            j++;
+	            k++;
+	        }
+	    }
+	 
+	  
+	    void sort(int arr[], int l, int r)
+	    {
+	        if (l < r)
+	        {
+	            // Find the middle point
+	            int m = (l+r)/2;
+	 
+	            // Sort first and second halves
+	            sort(arr, l, m);
+	            sort(arr , m+1, r);
+	 
+	            // Merge the sorted halves
+	            merge(arr, l, m, r);
+	        }
+	    }
 		public static void quickSortRight(int[] arr, int low, int high) {
 		if (arr == null || arr.length == 0)
 			return;
